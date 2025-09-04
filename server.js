72622b90e74d0a462b2037317160dfa99f4fdb00
mhamidjamil <55445517+mhamidjamil@users.noreply.github.com>
2025-09-04 12:44:07 +0500

    Change server listen address to '0.0.0.0' for external accessibility

--- server.js
+++ server.js
@@ -217,6 +217,6 @@ app.post('/send', async (req, res) => {
 });
 
 // Start the Express server
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0',() => {
+	console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
